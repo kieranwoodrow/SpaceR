3a3754ffa71d2c41e0ac436228c2dfa2799093cf
kieranwoodrow <kwoodrow@jhb.dvt.co.za>
2022-02-28 12:36:04 +0200

    clean up of comments

--- spaceR/spaceR/Utilities/Constants.swift
+++ spaceR/spaceR/Utilities/Constants.swift
@@ -8,9 +8,8 @@
 import Foundation
 
 struct Constants{
-    //Endpoint URl for getting ALL rockets
+    
     static let getAllRocketsUrl = URL(string: "https://api.spacexdata.com/v4/rockets")
-    //Endpoint URl for getting ALL rocket launches
     static let getAllLaunchesUrl = URL(string: "https://api.spacexdata.com/v5/launches")
 }
 
--- spaceR/spaceR/Utilities/EndPoints.swift
+++ spaceR/spaceR/Utilities/EndPoints.swift
@@ -18,46 +18,46 @@ extension URLSession{
     
     //Custom function that we want to now use
     func getAllRocketsEndpointURL <Generic: Codable>(url: URL?,
-                                                    model: Generic.Type,
-                                                    completion: @escaping (Result<Generic, Error>) -> Void){
-         
-            //Ensuring the URL is safe and we can use it
-            guard let endpointUrl = url else {
-                completion(.failure(CustomError.invalidUrl))
+                                                     model: Generic.Type,
+                                                     completion: @escaping (Result<Generic, Error>) -> Void){
+        
+        //Ensuring the URL is safe and we can use it
+        guard let endpointUrl = url else {
+            completion(.failure(CustomError.invalidUrl))
+            return
+        }
+        
+        //This task processes the url & simple cancelable asynchronous interface to receiving data
+        let apiTask = self.dataTask(with: endpointUrl) { data, _, error in
+            
+            //Ensures that the data recieved is safe to use
+            guard let safeData = data else {
+                
+                //Confirms if there is an error else custom error we created
+                if let error = error{
+                    completion(.failure(error))
+                }else{
+                    completion(.failure(CustomError.invalidData))
+                }
                 return
             }
-        
-            //This task processes the url & simple cancelable asynchronous interface to receiving data
-            let apiTask = self.dataTask(with: endpointUrl) { data, _, error in
+            
+            do{
+                //Runs JSON deserialization from the safe data into the generic model we pass in
+                let result = try JSONDecoder().decode(model, from: safeData)
                 
-                //Ensures that the data recieved is safe to use
-                guard let safeData = data else {
-                    
-                    //Confirms if there is an error else custom error we created
-                    if let error = error{
-                        completion(.failure(error))
-                    }else{
-                        completion(.failure(CustomError.invalidData))
-                    }
-                    return
+                //Tells the code we are complete and return what we deserialization
+                DispatchQueue.main.async {
+                    completion(.success(result))
                 }
                 
-                do{
-                    //Runs JSON deserialization from the safe data into the generic model we pass in
-                    let result = try JSONDecoder().decode(model, from: safeData)
-                    
-                    //Tells the code we are complete and return what we deserialization
-                    DispatchQueue.main.async {
-                        completion(.success(result))
-                    }
-                    
-                }catch{
-                    completion(.failure(error))
-                }
-                
+            }catch{
+                completion(.failure(error))
             }
-            //Resume is execute request in this instant
-            apiTask.resume()
+            
+        }
+        //Resume is execute request in this instant
+        apiTask.resume()
     }
 }
 
