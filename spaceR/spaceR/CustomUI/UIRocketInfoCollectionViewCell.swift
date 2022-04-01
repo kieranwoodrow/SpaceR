037aa1609afebb954da784d8470e659f2ad4cd1d
kieranwoodrow <kwoodrow@jhb.dvt.co.za>
2022-04-01 13:53:12 +0200

    created collection view cell file for styling

---
+++ spaceR/spaceR/CustomUI/UIRocketInfoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UIRocketInfoCollectionViewCell.swift
+//  SpaceR
+//
+//  Created by Kieran Woodrow on 2022/04/01.
+//
+
+import Foundation
+import UIKit
+
+class UIRocketInfoCollectionViewCell: UICollectionViewCell {
+    
+}
